7c8267d0bfd7a7efb4a5c31b8337f393cb8b9cf7
Andrey Kondalov <i@yukio.ru>
2020-05-01 15:24:26 +0400

    chore: update version

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cwebp = require('cwebp-bin');
 const program = require('commander');
 
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .option('-r, --recursive', 'Walk given directory recursively')
   .option('-f, --files <items>', 'Files to convert, split by \',\'', v => v.split(','))
   .option('-q, --quality <float>', 'Specify the compression factor for RGB channels between 0 and 100', 75)
